ddb7d9374391871f12a4af618eeb4ea808a16df2
mariasanudo <maria.rojas84@uabc.edu.mx>
2023-09-08 12:52:42 -0700

    Pandas jobs print funciones

--- pandas_jobs.py
+++ pandas_jobs.py
@@ -3,4 +3,5 @@ import pandas as pd
 jobs = pd.read_csv("datasets/Jobs.csv", index_col=0)
 #print(jobs.head(10)) #los primeros
 #print(jobs.sample(10)) #aleatorios
-print(jobs.describe()) #estadistica, resumen
+#print(jobs.describe()) #estadistica, resumen
+print(jobs.info) #tipo de datos

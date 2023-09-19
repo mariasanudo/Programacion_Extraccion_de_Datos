d2885be3725d6f373e2fee8c3778228cc8080150
mariasanudo <maria.rojas84@uabc.edu.mx>
2023-09-19 12:56:29 -0700

    mysql conection

---
+++ ejemplo.py
@@ -0,0 +1,5 @@
+msg = "hola como estas"
+print(msg.split(" ")[0])
+#########.replace("$"," ")
+# que reciba la busqueda y la cantidad de páginas
+# En caso de bloqueo hacer el time para escribir las cosas

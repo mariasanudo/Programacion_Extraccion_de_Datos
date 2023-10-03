c41eb8ee387992a58ef9dbd01df9557afc9324fc
mariasanudo <maria.rojas84@uabc.edu.mx>
2023-10-03 12:49:23 -0700

    mysql conection

--- pandas_mysql.py
+++ pandas_mysql.py
@@ -14,22 +14,24 @@ for item in DataBD:
 """
 
 cadena_conexion = f"mysql+mysqlconnector://{DataBD.USER.value}:{DataBD.PASSWORD.value}@{DataBD.SERVER.value}/{DataBD.NAME_BD.value}"
-print(cadena_conexion)
+#print(cadena_conexion)
 
 engine = create_engine(cadena_conexion)
 conexion = engine.connect()
-print(conexion)
+#print(conexion)
 
 data_oli = pd.read_csv("datasets/data_olimpiadas.csv", index_col=0)
-print(data_oli.sample(5))
+#print(data_oli.sample(5))
 
 gender = data_oli.gender.unique()
-print(gender)
+#print(gender)
 
 df_genders = pd.DataFrame(gender, columns=["nombre"])
-print(df_genders)
+#print(df_genders)
 #df_genders.to_sql("genero", conexion,if_exists= "append", index=False)
 
-query = "select nombre from genero"
-resultados = pd.read_sql(query, conexion)
+#query = "select nombre from genero where nombre = 'female'"
+query = "select nombre from genero where nombre = %s"
+parametros = ("female",)
+resultados = pd.read_sql(query, conexion, params=parametros)
 print(resultados)

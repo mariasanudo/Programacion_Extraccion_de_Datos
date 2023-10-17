f79dcc03ea4e73cd2a0a14c9d60ecf193b94065d
mariasanudo <maria.rojas84@uabc.edu.mx>
2023-10-17 12:48:30 -0700

    unidad3

--- Agrupamiento_de_datos.py
+++ Agrupamiento_de_datos.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 data = pd.read_csv("datasets/data_olimpiadas.csv",
@@ -10,4 +11,19 @@ columnas = ["gold","silver","bronze"]
 res = datos_agrupados[columnas].sum()
 #print(res)
 
-print(data.country.value_counts())
+#print(data.country.value_counts())
+#print(data.sort_values("year"))
+#print(data.sort_values("year",ascending=False))
+
+#print(datos_agrupados.mean(numeric_only=True))
+
+#print(data.describe().transpose())
+grupos = data.groupby(["gender","country"])
+grupos_gender = data.groupby("gender")
+#print(grupos_gender.sample(5))
+#print(grupos_gender.gold.mean())
+#print(grupos.gold.mean().unstack().transpose())
+#print(grupos.gold.mean().unstack())
+#print(grupos.gold.mean())
+
+print(data.pivot_table("gold", index="gender", columns="year", margins=True, aggfunc=np.sum))
